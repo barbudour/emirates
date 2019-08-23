9d0a20090333c515f220302cd310200724dedc26
Sergey Andreychenko <sergey.andreychenko@gmail.com>
2019-08-23 20:27:28 +0300

    comment dshare

--- data.js
+++ data.js
@@ -2,20 +2,20 @@ module.exports = {
     share: {
         default_img: "share.jpg",
         dynamic_shares: [
-            {
-                '/1': {
-                    img: '/images/1.png',
-                    title: 'Результат 1',
-                    description: 'Описание 1'
-                }
-            },
-            {
-                '/2': {
-                    img: '/images/2.png',
-                    title: 'Результат 2',
-                    description: 'Описание 2'
-                }
-            }
+            // {
+            //     '/1': {
+            //         img: '/images/1.png',
+            //         title: 'Результат 1',
+            //         description: 'Описание 1'
+            //     }
+            // },
+            // {
+            //     '/2': {
+            //         img: '/images/2.png',
+            //         title: 'Результат 2',
+            //         description: 'Описание 2'
+            //     }
+            // }
         ]
     }
 }
