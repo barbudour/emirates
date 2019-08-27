d1b4482b12f3a9f855f15b307dab97e547942417
Sergey Andreychenko <sergey.andreychenko@gmail.com>
2019-08-27 13:48:30 +0300

    DSHare fix

--- src/router.js
+++ src/router.js
@@ -13,7 +13,8 @@ let routes = [
 if (Object.keys(data.share.dynamic_shares).length > 0) {
   Object.keys(data.share.dynamic_shares).forEach((el) => {
     routes.push({
-      path: el,
+      path: '/' + el,
+      name: el
     })
   })
 
@@ -28,16 +29,16 @@ const router = new Router({
 
 if (Object.keys(data.share.dynamic_shares).length > 0) {
   router.beforeEach((to, from, next) => {
-    if (data.share.dynamic_shares[to.path] === undefined || data.share.dynamic_shares[to.path] === null) {
+    if (data.share.dynamic_shares[to.name] === undefined || data.share.dynamic_shares[to.name] === null) {
       document.querySelector('meta[property="og:image"]')['content'] = data.share.default_img
       document.querySelector('meta[property="og:title"]')['content'] = data.share.title
       document.querySelector('meta[property="og:description"]')['content'] = data.share.description
       document.querySelector('meta[name="twitter:image"]')['content'] = data.share.default_img
     } else {
-      document.querySelector('meta[property="og:image"]')['content'] = data.share.dynamic_shares[to.path].img
-      document.querySelector('meta[property="og:title"]')['content'] = data.share.dynamic_shares[to.path].title
-      document.querySelector('meta[property="og:description"]')['content'] = data.share.dynamic_shares[to.path].description
-      document.querySelector('meta[name="twitter:image"]')['content'] = data.share.dynamic_shares[to.path].img
+      document.querySelector('meta[property="og:image"]')['content'] = data.share.dynamic_shares[to.name].img
+      document.querySelector('meta[property="og:title"]')['content'] = data.share.dynamic_shares[to.name].title
+      document.querySelector('meta[property="og:description"]')['content'] = data.share.dynamic_shares[to.name].description
+      document.querySelector('meta[name="twitter:image"]')['content'] = data.share.dynamic_shares[to.name].img
     }
     next()
   });
