8e40fc9b6f7549247c499fa85c3c5fa2f7dc3ad9
Sergey Andreychenko <sergey.andreychenko@gmail.com>
2019-08-27 10:08:23 +0300

    fix router

--- src/router.js
+++ src/router.js
@@ -1,55 +1,46 @@
-"use strict";
+import Vue from 'vue'
+import Router from 'vue-router'
+const data = require('../data')
 
-exports.__esModule = true;
-exports.default = void 0;
+Vue.use(Router)
 
-var _vue = _interopRequireDefault(require("vue"));
-
-var _vueRouter = _interopRequireDefault(require("vue-router"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var data = require('../data');
-
-_vue.default.use(_vueRouter.default);
-
-var routes = [
+let routes = [
   {
-    path: '/'
+    path: '/',
   }
 ];
 
 if (data.share.dynamic_shares.length > 0) {
-  data.share.dynamic_shares.forEach(function (el) {
+  data.share.dynamic_shares.forEach((el) => {
     routes.push({
-      path: Object.keys(el)[0]
-    });
-  });
+      path: Object.keys(el)[0],
+    })
+  })
+
 }
 
-var router = new _vueRouter.default({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: routes
-});
+  routes
+})
 
 if (data.share.dynamic_shares.length > 0) {
-  router.beforeEach(function (to, from, next) {
+  router.beforeEach((to, from, next) => {
     if (data.share.dynamic_shares[to] === undefined || data.share.dynamic_shares[to] === null) {
-      document.querySelector('meta[property="og:image"]')['content'] = '/' + data.share.default_img;
-      document.querySelector('meta[property="og:title"]')['content'] = '/' + data.share.title;
-      document.querySelector('meta[property="og:description"]')['content'] = '/' + data.share.description;
-      document.querySelector('meta[name="twitter:image"]')['content'] = '/' + data.share.default_img;
+      document.querySelector('meta[property="og:image"]')['content'] = '/' + data.share.default_img
+      document.querySelector('meta[property="og:title"]')['content'] = '/' + data.share.title
+      document.querySelector('meta[property="og:description"]')['content'] = '/' + data.share.description
+      document.querySelector('meta[name="twitter:image"]')['content'] = '/' + data.share.default_img
     } else {
-      document.querySelector('meta[property="og:image"]')['content'] = '/' + data.share.dynamic_shares[to].img;
-      document.querySelector('meta[property="og:title"]')['content'] = '/' + data.share.dynamic_shares[to].title;
-      document.querySelector('meta[property="og:description"]')['content'] = '/' + data.share.dynamic_shares[to].description;
-      document.querySelector('meta[name="twitter:image"]')['content'] = '/' + data.share.dynamic_shares[to].img;
+      document.querySelector('meta[property="og:image"]')['content'] = '/' + data.share.dynamic_shares[to].img
+      document.querySelector('meta[property="og:title"]')['content'] = '/' + data.share.dynamic_shares[to].title
+      document.querySelector('meta[property="og:description"]')['content'] = '/' + data.share.dynamic_shares[to].description
+      document.querySelector('meta[name="twitter:image"]')['content'] = '/' + data.share.dynamic_shares[to].img
     }
-
-    next();
+    next()
   });
 }
 
-var _default = router;
-exports.default = _default;
+
+export default router
