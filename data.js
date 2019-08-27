4196816291ba9af2937c28f6f6b510bd5cc99141
Sergey Andreychenko <sergey.andreychenko@gmail.com>
2019-08-27 23:43:01 +0300

    analytics upg

--- data.js
+++ data.js
@@ -1,22 +1,25 @@
 module.exports = {
+    title: "Заголовок страницы",
     share: {
         default_img: "/share.jpg",
+        title: "Заголовок Share",
+        description: "Описание Share",
         dynamic_shares:
         {
             // 1: {
             //     img: '/share/share_1.jpg',
-            //     title: 'Результат 1',
-            //     description: 'Описание 1'
+            //     title: 'Супрематизм или Supreme',
+            //     description: 'Кажется, абстрактная живопись не ваша сильная сторона'
             // },
             // 2: {
             //     img: '/share/share_2.jpg',
-            //     title: 'Результат 2',
-            //     description: 'Описание 2'
+            //     title: 'Ретроград в авангарде',
+            //     description: 'Про супрематизм и футуризм вы возможно слышали'
             // },
             // 3: {
             //     img: '/share/share_3.jpg',
-            //     title: 'Результат 2',
-            //     description: 'Описание 2'
+            //     title: 'Магистр авангардных искусств',
+            //     description: 'Ваш следующий шаг - собственная выставка'
             // }
         }
 
